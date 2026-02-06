6c66da287fd70c9553148abbd4139d1ba679452a
SaraMsoso <Sara.soder44@gmail.com>
2026-02-06 23:23:26 +0100

    Implemented plugin registration function for Fastify

---
+++ src/plugins/index.ts
@@ -0,0 +1,9 @@
+import { FastifyInstance } from "fastify";
+import { cookiePlugin } from "./cookie";
+import { corsPlugin } from "./cors";
+
+// Function to register all plugins in Fastify
+export async function registerPlugins(app: FastifyInstance) {
+  await cookiePlugin(app);
+  await corsPlugin(app);
+}
