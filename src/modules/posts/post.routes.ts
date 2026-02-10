84f966cf0534fef5b84eed170644040c035f7f81
SaraMsoso <Sara.soder44@gmail.com>
2026-02-10 13:50:46 +0100

    Formatting the code and return proper 204 response for DELETE /posts/:id

--- src/modules/posts/post.routes.ts
+++ src/modules/posts/post.routes.ts
@@ -5,24 +5,35 @@ import { authGuard } from "../../middleware/authGuard";
 // Define routes for post-related operations: get all posts, get post by ID, create post, update post, delete post
 export async function postRoutes(app: FastifyInstance) {
   // GET /posts to retrieve all posts
-  app.get("/", async () => controller.getPosts());
+  app.get("/", async (_req, reply) => {
+    const posts = await controller.getPosts();
+    reply.send(posts);
+  });
+
   // GET /posts/:id to retrieve a single post by ID
-  app.get("/:id", async (req) =>
-    controller.getPost((req.params as any).id)
-  );
+  app.get("/:id", async (req, reply) => {
+    const post = await controller.getPost((req.params as any).id);
+    reply.send(post);
+  });
 
   // POST /posts to create a new post (protected route)
-  app.post("/", { preHandler: authGuard }, async (req) =>
-    controller.createPost(req.body)
-  );
+  app.post("/", { preHandler: authGuard }, async (req, reply) => {
+    const post = await controller.createPost(req.body);
+    reply.code(201).send(post);
+  });
 
   // PUT /posts/:id to update an existing post by ID (protected route)
-  app.put("/:id", { preHandler: authGuard }, async (req) =>
-    controller.updatePost((req.params as any).id, req.body)
-  );
+  app.put("/:id", { preHandler: authGuard }, async (req, reply) => {
+    const updatedPost = await controller.updatePost(
+      (req.params as any).id,
+      req.body
+    );
+    reply.send(updatedPost);
+  });
 
   // DELETE /posts/:id to delete a post by ID (protected route)
-  app.delete("/:id", { preHandler: authGuard }, async (req) =>
-    controller.deletePost((req.params as any).id)
-  );
+  app.delete("/:id", { preHandler: authGuard }, async (req, reply) => {
+    await controller.deletePost((req.params as any).id);
+    reply.code(204).send();
+  });
 }
